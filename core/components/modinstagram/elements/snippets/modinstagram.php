8a7ee750921ed611a8bdd69e68dd249424a9bb19
ig0r74 <ig0r74@yandex.ru>
2019-04-29 20:57:11 +0300

    Cache integrated to snippet

--- core/components/modinstagram/elements/snippets/modinstagram.php
+++ core/components/modinstagram/elements/snippets/modinstagram.php
@@ -2,11 +2,14 @@
 $access_token = $modx->getOption('accessToken', $scriptProperties, $modx->getOption('modinstagram_acess_token'), true);
 $tplWrapper = $modx->getOption('tplWrapper', $scriptProperties, false);
 $tpl = $modx->getOption('tpl', $scriptProperties, 'tpl.modInstagram.item');
+$cacheTime = $modx->getOption('cacheTime', $scriptProperties, 1800, true);
 $limit = $modx->getOption('limit', $scriptProperties, 20, true);
 $toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 $showLog = $modx->getOption('showLog', $scriptProperties, false);
 $maxId = $modx->getOption('maxId', $scriptProperties, false);
 $minId = $modx->getOption('minId', $scriptProperties, false);
+
+$cacheManager = $modx->getCacheManager();
 
 if (!$access_token) {
     $modx->log(MODX_LOG_LEVEL_ERROR, 'No ACCESS TOKEN');
@@ -17,52 +20,58 @@ $pdo = $modx->getService('pdoTools');
 
 $pdo->addTime('pdoTools loaded');
 
-$query = array(
-    'access_token' => $access_token,
-    'count' => $limit,
-    'max_id' => $maxId,
-    'min_id' => $minId
-);
-
-$response = file_get_contents('https://api.instagram.com/v1/users/self/media/recent/?' . http_build_query($query));
-
-if ($http_response_header[0] != 'HTTP/1.1 200 OK') {
-    $modx->log(MODX_LOG_LEVEL_ERROR, 'modInstagram error: ' . $http_response_header[0]);
-    return false;
-}
-
-$output = '';
-
-$response = json_decode($response, true);
-
-$idx = 1;
-
-if (!empty($response['data'])) {
-    foreach($response['data'] as $row) {
-        $output .= $pdo->getChunk($tpl, array(
-            'idx' => $idx,
-            'id' => $row['id'],
-            'image_thumbnail' => $row['images']['thumbnail']['url'],
-            'image_low_resolution' => $row['images']['low_resolution']['url'],
-            'image_standard_resolution' => $row['images']['standard_resolution']['url'],
-            'created_time' => $row['created_time'],
-            'caption_text' => $row['caption']['text'],
-            'likes_count' => $row['likes']['count'],
-            'comments_count' => $row['comments']['count'],
-            'type' => $row['type'],
-            'link' => $row['link'],
-            'location_name' => $row['location']['name'],
-            'video_standard_resolution' => $row['videos']['standard_resolution']['url'],
-            'video_low_bandwidth' => $row['videos']['low_bandwidth']['url'],
-            'video_low_resolution' => $row['videos']['low_resolution']['url'],
-            'carousel' => $row['carousel_media'],
-        ));
-        $idx++;
+if (!$output = $cacheManager->get('mod_instagram')) {
+    
+    $query = array(
+        'access_token' => $access_token,
+        'count' => $limit,
+        'max_id' => $maxId,
+        'min_id' => $minId
+    );
+    
+    $response = file_get_contents('https://api.instagram.com/v1/users/self/media/recent/?' . http_build_query($query));
+    
+    if ($http_response_header[0] != 'HTTP/1.1 200 OK') {
+        $modx->log(MODX_LOG_LEVEL_ERROR, 'modInstagram error: ' . $http_response_header[0]);
+        return false;
     }
-} else {
-    return false;
+    
+    $pdo->addTime('File received');
+    
+    $output = '';
+    
+    $response = json_decode($response, true);
+    
+    $idx = 1;
+    
+    if (!empty($response['data'])) {
+        foreach($response['data'] as $row) {
+            $output .= $pdo->getChunk($tpl, array(
+                'idx' => $idx,
+                'id' => $row['id'],
+                'image_thumbnail' => $row['images']['thumbnail']['url'],
+                'image_low_resolution' => $row['images']['low_resolution']['url'],
+                'image_standard_resolution' => $row['images']['standard_resolution']['url'],
+                'created_time' => $row['created_time'],
+                'caption_text' => $row['caption']['text'],
+                'likes_count' => $row['likes']['count'],
+                'comments_count' => $row['comments']['count'],
+                'type' => $row['type'],
+                'link' => $row['link'],
+                'location_name' => $row['location']['name'],
+                'video_standard_resolution' => $row['videos']['standard_resolution']['url'],
+                'video_low_bandwidth' => $row['videos']['low_bandwidth']['url'],
+                'video_low_resolution' => $row['videos']['low_resolution']['url'],
+                'carousel' => $row['carousel_media'],
+            ));
+            $idx++;
+        }
+    } else {
+        return false;
+    }
+    
+    $cacheManager->set('mod_instagram', $output, $cacheTime);
 }
-
 
 if (!empty($tplWrapper)) {
     $output = $pdo->getChunk($tplWrapper, array('output' => $output));
